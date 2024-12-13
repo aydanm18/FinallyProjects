1a99bfb761812a223a4bfed3e0e7e62e867532a4
Aydan <babayevaaydan18@gmail.com>
2024-12-13 16:56:27 +0400

    update use cors

--- FinalProject/server/app.js
+++ FinalProject/server/app.js
@@ -6,7 +6,16 @@ const route =require('./routes/index')
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
+const allowedOrigins = [
+    "https://client-code-final.vercel.app/",   // Client frontend URL
+    "https://admin-code-final.vercel.app/"     // Admin frontend URL
+  ];
+  
+  app.use(cors({
+    origin: allowedOrigins,  // Allow both client and admin frontends
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }));
 
 app.use(route.bloks)
 app.use(route.reservations)
@@ -16,5 +25,8 @@ app.use(route.teams)
 app.use(route.menues)
 app.use(route.messages)
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the deployed project!");
+  });
 
- 
+module.exports = app
